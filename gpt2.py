9dc5d82f897fedd099f628f704da37b6a0d885ae
Maksim Penkin <penkin97@gmail.com>
2025-10-24 16:52:27 +0300

    gpt-2 block in implemented

--- gpt2.py
+++ gpt2.py
@@ -3,6 +3,34 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+
+
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.attn = CausalSelfAttention(config)
+        self.mlp = MLP(config)
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
 @dataclass
 class GPTConfig:
     block_size: int = 256
