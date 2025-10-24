6a5de6b9050d615797b362c38863c79ed91368c5
Maksim Penkin <penkin97@gmail.com>
2025-10-24 10:31:24 +0300

    mha added as a simple multiple heads of self-attention in parallel

--- v2.py
+++ v2.py
@@ -81,6 +81,19 @@ class Head(nn.Module):
         out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
         return out
 
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel"""
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(num_heads * head_size, n_embd)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
